102842a1944c7c23f62b39144f6bc771c656f2af
dbrugger946 <nope@nope2020.com>
2025-04-09 14:58:17 -0400

    updated settings

---
+++ vllm-dev/query3.sh
@@ -0,0 +1,6 @@
+curl -H "Content-Type: application/json" https://granite-30-8b-instruct-aiworkshop.example.com -d '{
+  "model": "granite-30-8b-instruct",
+  "messages": [
+    {"role": "users", "content": "How are you today?"}
+  ]
+}'
