73cfcdff4015ac008d684c2d552b6d8ddf4ae29c
dbrugger946 <nope@nope2020.com>
2025-04-22 13:25:22 -0400

    updates to vllm

--- vllm-dev/run-vllm-container-embedded.sh
+++ vllm-dev/run-vllm-container-embedded.sh
@@ -1,10 +1,11 @@
 podman run --rm \
+             --name=granite-embedding \
              --privileged=true \
              --shm-size=4g \
 	         -v ~/.cache/huggingface:/root/.cache/huggingface \
              -p 8000:8000 \
              -e VLLM_CPU_KVCACHE_SPACE=30  \
              -e VLLM_CPU_OMP_THREADS_BIND=4 \
-             public.ecr.aws/q9t5s3a7/vllm-cpu-release-repo:v0.8.4 \
-             --model=ibm-granite/granite-3.3-2b-instruct \
+             vllm-cpu-env:latest \
+             --model=ibm-granite/granite-embedding-30m-english \
              --dtype=bfloat16 \
