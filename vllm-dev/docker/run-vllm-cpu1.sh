b44eb09cc14ccc944ffd09399620fd011d32cc45
dbrugger946 <you@example.com>
2025-06-03 13:04:56 -0400

    updated vllm layout

---
+++ vllm-dev/docker/run-vllm-cpu1.sh
@@ -0,0 +1,10 @@
+docker run --rm \
+             --privileged=true \
+             --shm-size=4g \
+	         -v ~/.cache/huggingface:/root/.cache/huggingface \
+             -p 8000:8000 \
+             -e VLLM_CPU_KVCACHE_SPACE=30  \
+             -e VLLM_CPU_OMP_THREADS_BIND=6 \
+             public.ecr.aws/q9t5s3a7/vllm-cpu-release-repo:v0.9.0 \
+             --model=ibm-granite/granite-3.3-2b-instruct \
+             --dtype=bfloat16 
---
+++ vllm-dev/docker/run-vllm-gpu1.sh
@@ -0,0 +1,6 @@
+docker run --runtime nvidia --gpus all \
+    -v ~/.cache/huggingface:/root/.cache/huggingface \
+    -p 8000:8000 \
+    --ipc=host \
+    vllm/vllm-openai:v0.9.0 \
+    --model=ibm-granite/granite-3.3-2b-instruct
