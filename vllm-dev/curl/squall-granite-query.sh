0bd0d2feb3e4cf636dad8161333ec136d6965251
Your Name <you@example.com>
2025-07-18 13:09:59 -0400

    updates

--- vllm-dev/curl/squall-granite-query.sh
+++ vllm-dev/curl/squall-granite-query.sh
@@ -1,7 +1,7 @@
-curl http://localhost:8000/v1/chat/completions \
+curl http://192.168.0.13:8000/v1/chat/completions \
     -H "Content-Type: application/json" \
     -d '{
-        "model": "ibm-granite/granite-3.2-2b-instruct",
+        "model": "ibm-granite/granite-3.3-2b-instruct",
 	"messages": [
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": "Who won the world series in 2020?"}
